8d504f99776a70fd95a3289ae340f0407767c31e
Morteza Reisi <Morteza.reisib@gmail.com>
2024-08-23 03:10:48 +0330

    OK. I Don't know. WTF! It's Working!

--- code.ts
+++ code.ts
@@ -1,34 +1,53 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+async function fetchCollections() {
+  const collections = await figma.variables.getLocalVariableCollectionsAsync();
+  return collections.map(collection => ({
+    id: collection.id,
+    name: collection.name
+  }));
+}
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+async function exportVariables(collectionId: string) {
+  const collection = await figma.variables.getVariableCollectionByIdAsync(collectionId);
+  if (!collection) return {};
 
-// This shows the HTML page in "ui.html".
+  const variables = await figma.variables.getLocalVariablesAsync();
+  const variablesByCollection = variables
+    .filter(variable => variable.collectionId === collectionId)
+    .map(variable => ({
+      id: variable.id,
+      name: variable.name,
+      type: variable.type,
+      value: variable.value
+    }));
+
+  return {
+    [collection.name]: {
+      modes: {
+        "Mode 1": variablesByCollection.reduce((acc, variable) => {
+          acc[variable.name] = {
+            "$scopes": ["ALL_SCOPES"],
+            "$type": variable.type,
+            "$value": variable.value
+          };
+          return acc;
+        }, {})
+      }
+    }
+  };
+}
+
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createEllipse();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}, boundVariables: {}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === 'fetchCollections') {
+    const collections = await fetchCollections();
+    figma.ui.postMessage({ type: 'collections', collections });
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  if (msg.type === 'export') {
+    const { collection } = msg.pluginMessage;
+    const jsonData = await exportVariables(collection);
+    const jsonString = JSON.stringify(jsonData, null, 2);
+    figma.ui.postMessage({ type: 'file', json: jsonString });
+  }
 };
